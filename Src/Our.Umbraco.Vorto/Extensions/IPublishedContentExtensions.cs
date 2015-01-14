1b008275f3a95df89ac30417932fb7a3a48401c6
Allyen <allyen@seznam.cz>
2015-01-14 13:53:08 +0100

    Allow GetVortoValue to be called on property value, not only on whole content (for archetype integration)

--- Src/Our.Umbraco.Vorto/Extensions/IPublishedContentExtensions.cs
+++ Src/Our.Umbraco.Vorto/Extensions/IPublishedContentExtensions.cs
@@ -97,62 +97,13 @@ namespace Our.Umbraco.Vorto.Extensions
 				var prop = content.GetProperty(propertyAlias, recursive);
 				if (prop.Value is VortoValue)
 				{
-					// Get the serialized value
-					var vortoModel = prop.Value as VortoValue;
-					var value = vortoModel.Values[cultureName];
-
-					// If the value is of type T, just return it
-					//if (value is T)
-					//	return (T)value;
-
-					// Get target datatype
-					var targetDataType = VortoHelper.GetTargetDataTypeDefinition(vortoModel.DtdGuid);
-
-					// Umbraco has the concept of a IPropertyEditorValueConverter which it 
-					// also queries for property resolvers. However I'm not sure what these
-					// are for, nor can I find any implementations in core, so am currently
-					// just ignoring these when looking up converters.
-					// NB: IPropertyEditorValueConverter not to be confused with
-					// IPropertyValueConverter which are the ones most people are creating
-					var properyType = CreateDummyPropertyType(targetDataType.Id, targetDataType.PropertyEditorAlias);
-					var converters = PropertyValueConvertersResolver.Current.Converters.ToArray();
-
-					// In umbraco, there are default value converters that try to convert the 
-					// value if all else fails. The problem is, they are also in the list of
-					// converters, and the means for filtering these out is internal, so
-					// we currently have to try ALL converters to see if they can convert
-					// rather than just finding the most appropreate. If the ability to filter
-					// out default value converters becomes public, the following logic could
-					// and probably should be changed.
-					foreach (var converter in converters.Where(x => x.IsConverter(properyType)))
-					{
-						// Convert the type using a found value converter
-						var value2 = converter.ConvertDataToSource(properyType, value, false);
-
-						// If the value is of type T, just return it
-						if (value2 is T)
-							return (T)value2;
-
-						// Value is not final value type, so try a regular type conversion aswell
-						var convertAttempt = value2.TryConvertTo<T>();
-						if (convertAttempt.Success)
-							return convertAttempt.Result;
-					}
-
-					// Value is not final value type, so try a regular type conversion
-					var convertAttempt2 = value.TryConvertTo<T>();
-					if (convertAttempt2.Success)
-						return convertAttempt2.Result;
-
-					return default(T);
+                    return Vorto.GetVortoValue<T>(prop.Value, cultureName, defaultValue);
 				}
 				
 				if (prop.Value is T)
 				{
 					return prop.Value as T;
 				}
-				
-				return default(T);
 			}
 
 			return defaultValue;
@@ -160,13 +111,6 @@ namespace Our.Umbraco.Vorto.Extensions
 		
 		#endregion
 
-		private static PublishedPropertyType CreateDummyPropertyType(int dataTypeId, string propertyEditorAlias)
-		{
-			return new PublishedPropertyType(null,
-				new PropertyType(new DataTypeDefinition(-1, propertyEditorAlias)
-				{
-					Id = dataTypeId
-				}));
-		}
+		
 	}
 }
--- Src/Our.Umbraco.Vorto/Vorto.cs
+++ Src/Our.Umbraco.Vorto/Vorto.cs
@@ -1,6 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Our.Umbraco.Vorto.Helpers;
 using Our.Umbraco.Vorto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Umbraco.Core;
+using Umbraco.Core.Models;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
 
 namespace Our.Umbraco.Vorto
 {
@@ -9,6 +16,75 @@ namespace Our.Umbraco.Vorto
 		#region Event Handlers
 
 		public static event EventHandler<FilterLanguagesEventArgs> FilterLanguages;
+
+        public static T GetVortoValue<T>(object value, string cultureName = null, T defaultValue = default(T))
+        {
+            if (cultureName == null)
+                cultureName = Thread.CurrentThread.CurrentUICulture.Name;
+
+            if (value is VortoValue)
+            {
+                // Get the serialized value
+                var vortoModel = value as VortoValue;
+                var langValue = vortoModel.Values[cultureName];
+
+                // If the value is of type T, just return it
+                //if (value is T)
+                //	return (T)value;
+
+                // Get target datatype
+                var targetDataType = VortoHelper.GetTargetDataTypeDefinition(vortoModel.DtdGuid);
+
+                // Umbraco has the concept of a IPropertyEditorValueConverter which it 
+                // also queries for property resolvers. However I'm not sure what these
+                // are for, nor can I find any implementations in core, so am currently
+                // just ignoring these when looking up converters.
+                // NB: IPropertyEditorValueConverter not to be confused with
+                // IPropertyValueConverter which are the ones most people are creating
+                var properyType = CreateDummyPropertyType(targetDataType.Id, targetDataType.PropertyEditorAlias);
+                var converters = PropertyValueConvertersResolver.Current.Converters.ToArray();
+
+                // In umbraco, there are default value converters that try to convert the 
+                // value if all else fails. The problem is, they are also in the list of
+                // converters, and the means for filtering these out is internal, so
+                // we currently have to try ALL converters to see if they can convert
+                // rather than just finding the most appropreate. If the ability to filter
+                // out default value converters becomes public, the following logic could
+                // and probably should be changed.
+                foreach (var converter in converters.Where(x => x.IsConverter(properyType)))
+                {
+                    // Convert the type using a found value converter
+                    var value2 = converter.ConvertDataToSource(properyType, langValue, false);
+
+                    // If the value is of type T, just return it
+                    if (value2 is T)
+                        return (T)value2;
+
+                    // Value is not final value type, so try a regular type conversion aswell
+                    var convertAttempt = value2.TryConvertTo<T>();
+                    if (convertAttempt.Success)
+                        return convertAttempt.Result;
+                }
+
+                // Value is not final value type, so try a regular type conversion
+                var convertAttempt2 = langValue.TryConvertTo<T>();
+                if (convertAttempt2.Success)
+                    return convertAttempt2.Result;
+
+                return defaultValue;
+            }
+
+            return defaultValue;
+        }
+
+        private static PublishedPropertyType CreateDummyPropertyType(int dataTypeId, string propertyEditorAlias)
+        {
+            return new PublishedPropertyType(null,
+                new PropertyType(new DataTypeDefinition(-1, propertyEditorAlias)
+                {
+                    Id = dataTypeId
+                }));
+        }
 
 		internal static void CallFilterLanguages(FilterLanguagesEventArgs args)
 		{
@@ -26,7 +102,7 @@ namespace Our.Umbraco.Vorto
 		public int CurrentPageId { get; set; }
 		public int ParentPageId { get; set; }
 
-		public IList<Language> Languages { get; set; }
+		public IList<Our.Umbraco.Vorto.Models.Language> Languages { get; set; }
 	}
 
 	#endregion
