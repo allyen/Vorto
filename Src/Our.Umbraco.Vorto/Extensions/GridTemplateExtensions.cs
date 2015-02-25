fced1411b4705691b3772d36fa2f557d4c3e88e2
Allyen <allyen@seznam.cz>
2015-02-25 12:44:31 +0100

    Vorto for grid

---
+++ Src/Our.Umbraco.Vorto/Extensions/GridTemplateExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.Web.Routing;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+
+namespace Our.Umbraco.Vorto.Extensions
+{
+    public static class GridTemplateExtensions
+    {
+        public static MvcHtmlString GetVortoGridHtml<T>(this IPublishedContent contentItem, HtmlHelper html)
+        {
+            return GetVortoGridHtml(contentItem, html, "bodyText", "bootstrap3");
+        }
+
+        public static MvcHtmlString GetVortoGridHtml(this IPublishedContent contentItem, HtmlHelper html, string propertyAlias)
+        {
+            return GetVortoGridHtml(contentItem, html, propertyAlias, "bootstrap3");
+        }
+
+        public static MvcHtmlString GetVortoGridHtml(this IPublishedContent contentItem, HtmlHelper html, string propertyAlias, string framework)
+        {
+            var view = "Grid/" + framework;
+            var model = contentItem.GetVortoValue(propertyAlias);
+
+            return html.Partial(view, model, new ViewDataDictionary());
+        }
+    }
+}
