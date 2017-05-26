3074c243531c90e4494b1980f14fb0694bbeb599
Pavel Budik <budik@ics.muni.cz>
2017-05-26 21:45:39 +0200

    Fix for missing language translations, removed unused code

--- Src/Our.Umbraco.Vorto/Vorto.cs
+++ Src/Our.Umbraco.Vorto/Vorto.cs
@@ -2,7 +2,6 @@
 using Our.Umbraco.Vorto.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using Umbraco.Core;
 using Umbraco.Core.Models;
@@ -27,23 +26,14 @@ namespace Our.Umbraco.Vorto
             {
                 // Get the serialized value
                 var vortoModel = value as VortoValue;
-                var langValue = vortoModel.Values[cultureName];
 
-                // If the value is of type T, just return it
-                //if (value is T)
-                //	return (T)value;
-
+                object langValue = null;
+                if (!vortoModel.Values.TryGetValue(cultureName, out langValue))
+                    return defaultValue;
+                
                 // Get target datatype
                 var targetDataType = VortoHelper.GetTargetDataTypeDefinition(dataTypeId);
-
-                // Umbraco has the concept of a IPropertyEditorValueConverter which it 
-                // also queries for property resolvers. However I'm not sure what these
-                // are for, nor can I find any implementations in core, so am currently
-                // just ignoring these when looking up converters.
-                // NB: IPropertyEditorValueConverter not to be confused with
-                // IPropertyValueConverter which are the ones most people are creating
                 var properyType = CreateDummyPropertyType(targetDataType.Id, targetDataType.PropertyEditorAlias, contentType);
-
 
                 var source = properyType.ConvertDataToSource(langValue, false);
                 var result = properyType.ConvertSourceToObject(source, false);
@@ -54,51 +44,12 @@ namespace Our.Umbraco.Vorto
                 if (convert.Success)
                     return convert.Result;
                 else
-                    return default(T);
-
-                var converters = PropertyValueConvertersResolver.Current.Converters.ToArray();
-
-                // In umbraco, there are default value converters that try to convert the 
-                // value if all else fails. The problem is, they are also in the list of
-                // converters, and the means for filtering these out is internal, so
-                // we currently have to try ALL converters to see if they can convert
-                // rather than just finding the most appropreate. If the ability to filter
-                // out default value converters becomes public, the following logic could
-                // and probably should be changed.
-                foreach (var converter in converters.Where(x => x.IsConverter(properyType)))
-                {
-                    // Convert the type using a found value converter
-                    var value2 = converter.ConvertDataToSource(properyType, langValue, false);
-
-                    // If the value is of type T, just return it
-                    if (value2 is T)
-                        return (T)value2;
-
-                    // Value is not final value type, so try a regular type conversion aswell
-                    var convertAttempt = value2.TryConvertTo<T>();
-                    if (convertAttempt.Success)
-                        return convertAttempt.Result;
-                }
-
-                // Value is not final value type, so try a regular type conversion
-                var convertAttempt2 = langValue.TryConvertTo<T>();
-                if (convertAttempt2.Success)
-                    return convertAttempt2.Result;
-
-                return defaultValue;
+                    return defaultValue;
             }
 
             return defaultValue;
         }
 
-        /*private static PublishedPropertyType CreateDummyPropertyType(int dataTypeId, string propertyEditorAlias, PublishedContentType contentType)
-        {
-            return new PublishedPropertyType(contentType,
-                new PropertyType(new DataTypeDefinition(-1, propertyEditorAlias)
-                {
-                    Id = dataTypeId
-                }));
-        }*/
         /// <summary>
         /// Creates dummy property type.
         /// </summary>
